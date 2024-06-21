73e53429d9a7fa6045bff514c493ccb5722f59a6
nicoacosta888 <enzonicolasacosta@gmail.com>
2024-06-21 18:28:59 -0300

    Commit 1 (Nico Acosta, 21/6/24)

---
+++ Project_Triathlon/src/model/Athlete.java
@@ -0,0 +1,21 @@
+package Project_Triathlon.src.model;
+
+import java.util.Date;
+
+public class Athlete {
+    private int num;
+    private String name;
+    private String id;
+    private String nationality;
+    private Date birthDate;
+    private String gender;
+    private double weight;
+    private double height;
+    private double percEndedRaces;
+    private double economicBudget;
+    private int ranking;
+    private PhysicalConditions stats;
+
+
+
+}
---
+++ Project_Triathlon/src/model/PhysicalConditions.java
@@ -0,0 +1,11 @@
+package Project_Triathlon.src.model;
+
+public class PhysicalConditions {
+    private double swimmingAptitude;
+    private double cyclismAptitude;
+    private double pedestrianismAptitude;
+    private double stamina;
+    private double mentalStrength;
+
+
+}
