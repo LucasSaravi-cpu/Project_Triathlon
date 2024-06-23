49e403f66ef23ed43f8c68e732ab7d460f309dcc
user <user@DESKTOP-N8NPIO1.fibertel.com.ar>
2024-06-23 18:13:37 -0300

    Cuarto comic ( lucas saravi 23/6/2024)

--- Project_Triathlon/src/model/WeatherConditions.java
+++ Project_Triathlon/src/model/WeatherConditions.java
@@ -7,6 +7,46 @@ public class WeatherConditions {
     private double swimmingweathering;
     private double cyclingweathering;
     private double pedestrianismweathering;
+	public MeasurementUnit getMeasurementUnit() {
+		return measurementUnit;
+	}
+	public void setMeasurementUnit(MeasurementUnit measurementUnit) {
+		this.measurementUnit = measurementUnit;
+	}
+	public double getLowertier() {
+		return lowertier;
+	}
+	public void setLowertier(double lowertier) {
+		this.lowertier = lowertier;
+	}
+	public double getUppertier() {
+		return uppertier;
+	}
+	public void setUppertier(double uppertier) {
+		this.uppertier = uppertier;
+	}
+	public double getSwimmingweathering() {
+		return swimmingweathering;
+	}
+	public void setSwimmingweathering(double swimmingweathering) {
+		this.swimmingweathering = swimmingweathering;
+	}
+	public double getCyclingweathering() {
+		return cyclingweathering;
+	}
+	public void setCyclingweathering(double cyclingweathering) {
+		this.cyclingweathering = cyclingweathering;
+	}
+	public double getPedestrianismweathering() {
+		return pedestrianismweathering;
+	}
+	public void setPedestrianismweathering(double pedestrianismweathering) {
+		this.pedestrianismweathering = pedestrianismweathering;
+	}
+    
+    
+    
+    
    
 
 }
