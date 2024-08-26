44a431e27f3b091fb3459ffe1efce9b510375ee1
alvar <alvarezjosematias1@gmail.com>
2024-08-26 10:10:47 -0300

    Commit 6 Jose Alvarez

--- Project_Triathlon/src/view/CustomWeatherPanel.java
+++ Project_Triathlon/src/view/CustomWeatherPanel.java
@@ -2,21 +2,21 @@ package view;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CustomWeatherPanel extends JFrame
 {
     private JPanel contentPane;
-    private JButton addButton;
     public CustomWeatherPanel()
     {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(977, 100, 578, 699);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setBounds(900, 100, 500, 500);
         setResizable(false);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
         contentPane.setLayout(null);
-        addButton = new JButton();
-        add(addButton);
+        setContentPane(contentPane);
     }
 }
+
