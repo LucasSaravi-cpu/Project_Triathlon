bf2171aa863f86e94353f3454f713bcb201da6e0
user <user@DESKTOP-N8NPIO1.fibertel.com.ar>
2024-10-20 19:55:49 -0300

    error commit

--- Project_Triathlon/src/view/AthletePanel.java
+++ Project_Triathlon/src/view/AthletePanel.java
@@ -9,6 +9,11 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class AthletePanel extends JPanel {
+	
+	
+	//------------------------------------------------>||ATTRIBUTES||<--------------------------------------------------------\\
+	
+	
     private JButton increase;
     private JButton decrease;
     private JLabel athleteLabel;
